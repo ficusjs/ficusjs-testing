70f9fca45769b5f0c7e0159ccee17a4cb59c8115
Matt Levy <ducksoupdev@gmail.com>
2021-04-20 11:41:54 +0100

    feat: make importer return optional promise

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,2 +1,2 @@
 export declare function init (): void
-export declare function render<T> (tagName: string, importer: () => Promise<{}>): T
+export declare function render<T> (tagName: string, importer: () => Promise<{}> | void): T
