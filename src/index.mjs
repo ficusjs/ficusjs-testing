5a0b3fc10af8e2c33d2b6655a13431f7ea74f96e
Matt Levy <ducksoupdev@gmail.com>
2021-04-20 10:31:36 +0100

    feat: initial commit of code

---
+++ src/index.mjs
@@ -0,0 +1,18 @@
+import { JSDOM } from 'jsdom'
+
+export function init () {
+  const dom = new JSDOM('<!DOCTYPE html><html><head></head><body></body></html>')
+  globalThis.dom = dom
+  globalThis.window = dom.window
+  globalThis.document = dom.window.document
+  globalThis.customElements = window.customElements
+  globalThis.HTMLElement = window.HTMLElement
+}
+
+export async function render (tagName, importer) {
+  await importer()
+  const document = globalThis.document
+  const basicComp = document.createElement(tagName)
+  document.body.appendChild(basicComp)
+  return basicComp
+}
---
+++ types/index.d.ts
@@ -0,0 +1,2 @@
+export declare function init (): void
+export declare function render (tagName: string, importer: () => Promise<void>): void
