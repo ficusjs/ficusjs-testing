b3b0f985801e4a0a38c930048d0369b26cb3a024
Matt Levy <ducksoupdev@gmail.com>
2021-05-01 16:06:04 +0100

    fix: add Node to global

--- src/index.mjs
+++ src/index.mjs
@@ -3,6 +3,7 @@ import { JSDOM } from 'jsdom'
 export function init () {
   const dom = new JSDOM('<!DOCTYPE html><html><head></head><body></body></html>')
   globalThis.dom = dom
+  globalThis.Node = dom.Node
   globalThis.window = dom.window
   globalThis.document = dom.window.document
   globalThis.customElements = window.customElements
